2bcd0af4e9780c5e5c0888d26ace5fc300119e47
fartem <jaman.smlnsk@gmail.com>
2020-07-04 16:02:02 +0300

    2020-07-04 Update: Updated screenshot script

--- .scripts/make_screenshot.rb
+++ .scripts/make_screenshot.rb
@@ -4,5 +4,10 @@ require 'date'
 
 date = DateTime.parse(Time.now.to_s).strftime('%d_%m_%Y_%H_%M_%S')
 
-`maim screenshot_#{date}.png`
+dest = './Pictures/Screenshots'
+if !Dir.exists?(dest)
+  Dir.mkdir(dest)
+end
+
+`maim #{dest}/screenshot_#{date}.png`
 
